136bfb4ed8192d7a4072a95aa797fa55c64ad888
Jeppe BLicher Tarp <raider.j@gmail.com>
2012-02-14 11:55:59 +0100

    Adding project for engine prototype, with some comments

---
+++ engine/prototype/External/Character.cpp
@@ -0,0 +1,18 @@
+/*
+ * Character.cpp
+ *
+ *  Created on: 14/02/2012
+ *      Author: Ezphares
+ */
+
+#include "Character.h"
+
+Character::Character() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Character::~Character() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ engine/prototype/program.cpp
@@ -0,0 +1,15 @@
+/*
+ * program.cpp
+ *
+ *  Created on: 14/02/2012
+ *      Author: Ezphares
+ */
+
+int main(int argc, const char *argv[])
+{
+	return 0;
+}
+
+
+
+
