951cdee5550fc8a9145db0037e85f5c68f16d263
Jeppe BLicher Tarp <raider.j@gmail.com>
2012-03-21 11:40:59 +0100

    Updated main to scan for testing

--- Compiler/Prototype/main.cpp
+++ Compiler/Prototype/main.cpp
@@ -4,9 +4,37 @@
  *  Created on: 19/03/2012
  *      Author: vixen
  */
-#include "FileReader.h"
 
-int main (int argc, char** argv)
+#include <vector>
+#include <string>
+#include "Scanner.h"
+
+using namespace lexer;
+using namespace std;
+
+int main (int argc, char* argv[])
 {
-    return 1;
+	vector<string> files;
+	Scanner *scanner = new Scanner();
+
+	for (int i = 1; i < argc; i++)
+	{
+		scanner->add_file(argv[i]);
+		files.push_back(argv[i]);
+	}
+
+	Token *tokens;
+
+	try
+	{
+		tokens = scanner->scan();
+	}
+	catch (char* error)
+	{
+	}
+
+	delete scanner;
+	delete tokens;
+
+	return 0;
 }
