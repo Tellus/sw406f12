aa4620e1afb60fdb07a071a2566e15c9585aa914
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2012-04-19 09:52:21 +0200

    Forgot to add the DuplicateKeyException files. Broken build, my bad.

---
+++ engine/BattleEngine/Source/System/Exceptions/DuplicateKeyException.cpp
@@ -0,0 +1,26 @@
+/*
+ * DuplicateKeyException.cpp
+ *
+ *  Created on: 18/04/2012
+ *      Author: Johannes
+ */ 
+
+#include "exceptions/DuplicateKeyException.h"
+ 
+namespace engine {
+
+DuplicateKeyException::DuplicateKeyException(std::string msg)
+{
+	this->msg = msg;
+	this->what();
+}
+
+DuplicateKeyException::~DuplicateKeyException() throw()
+{}
+
+const char* DuplicateKeyException::what() const throw()
+{
+	return this->msg.c_str();
+}
+
+}
