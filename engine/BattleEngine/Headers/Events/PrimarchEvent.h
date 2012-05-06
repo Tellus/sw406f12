febe0c9a4f8a8cf1951b57570c1e3f3253d273a9
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2012-05-06 23:18:20 +0200

    More event coding and VS2010 support.
    Write me if the vs2010 isn't working. I refrained from adding files that are user- (and build-) dependent to save on commits.
    
    I think I've got a decent (although by far not perfect) approach to simple signaling. I estimate it'll be done by tomorrow evening.
    
    Signed-off-by: Johannes Lindhart Borresen <johannes@the.homestead.dk>

---
+++ engine/BattleEngine/Headers/Events/PrimarchEvent.h
@@ -0,0 +1,24 @@
+/*
+ * PrimarchEvent.h
+ *
+ *  Created on: 06/05/2012
+ *      Author: Johannes
+ */
+
+#pragma once
+
+#include "GameEvent.h"
+
+namespace engine {
+
+class PrimarchEvent : public GameEvent
+{
+
+	/**
+	 * Returns "PRIMARCH;GENERIC";
+	 **/
+	std::list<std::string> get_type();
+
+};
+
+}
