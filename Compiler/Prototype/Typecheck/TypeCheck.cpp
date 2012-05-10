f8f1b0b3cf8ca6bbfbd7f99d2a86076a4176dc34
Jeppe BLicher Tarp <raider.j@gmail.com>
2012-05-10 15:23:41 +0200

    Meh

--- Compiler/Prototype/Typecheck/TypeCheck.cpp
+++ Compiler/Prototype/Typecheck/TypeCheck.cpp
@@ -11,22 +11,12 @@ using namespace parser;
 
 namespace typec {
 
-symbol* TypeCheck::traverse(ASTNode *ast)
-{
-	symbol* retsym = new symbol;
-
-
-
-	return retsym;
-}
-
 symbol* TypeCheck::lookup_symbol(std::string name, std::map<std::string, symbol*>* looktable)
 {
-	symbol* retsym = new symbol;
+	if (looktable->count(name) == 0)
+		return NULL;
 
-
-
-	return retsym;
+	return (*looktable)[name];
 }
 
 void TypeCheck::add_symbol(std::map<std::string, symbol*>* addtable, std::string name, std::map<std::string, symbol*>* reftable, assign_enum assign)
@@ -43,6 +33,11 @@ void TypeCheck::add_symbol(std::map<std::string, symbol*>* addtable, std::string
 
 void TypeCheck::initiate_table()
 {
+	// Declaration table
+	add_symbol(&DeclarationTable, "Character", &CharacterTable, ASSIGN_NONE);
+	add_symbol(&DeclarationTable, "Ability", &AbilityTable, ASSIGN_NONE);
+	add_symbol(&DeclarationTable, "Behaviour", &BehaviourTable, ASSIGN_NONE);
+	add_symbol(&DeclarationTable, "Event", &EventTable, ASSIGN_NONE);
 	//Character table initialized with all constant symbols
 	add_symbol(&CharacterTable, "name", NULL, ASSIGN_STRING);
 	add_symbol(&CharacterTable, "health", &ResourceTable, ASSIGN_NUMBER);
@@ -100,14 +95,58 @@ void TypeCheck::initiate_table()
 
 }
 //Traverse, decorate the ASTNodes with type information and perform basic type checking
-void TypeCheck::decorate(ASTNode *node)
+bool TypeCheck::decorate(ASTNode *node, std::map<std::string, symbol*> *scope)
 {
-	//maybe something to be done before looping
-	//Looping through nodes bottom-up
+	symbol *temp;
+	primitive_type type = T_NULL;
+
 	for (list<ASTNode*>::iterator i = node->children.begin(); i != node->children.end(); i++)
 	{
-		this->decorate(*i);
+		switch (node->type)
+		{
+			case(NODE_DECLARATION):
+				if ((*i)->type == NODE_SUBCLASS) // TODO For now, inheritance is disabled!
+				{
+					if (this->lookup_symbol((*i)->data, &(this->DeclarationTable)) != NULL)
+						return false; // Duplicate declaration
+				}
 
+				if ((*i)->type == NODE_SUPERCLASS)
+				{
+					temp = this->lookup_symbol((*i)->data, &(this->DeclarationTable));
+					if (temp == NULL)
+						return false; // Missing superclass
+
+					scope = temp->type;
+				}
+				break;
+			default:
+				break;
+		}
+
+		/*
+		 * Recursive Decoration call
+		 */
+		if (!this->decorate(*i, scope))
+			return false;
+
+		/*
+		 * Post Decoration checks
+		 */
+		switch(node->type)
+		{
+			case NODE_EXPRESSION:
+			case NODE_TERM:
+			case NODE_FACTOR:
+				if (type == T_NULL)
+					type = (*i)->dec->type;
+				else if (type != (*i)->dec->type && (*i)->dec->type != T_NULL)
+					return false; // Illegal operation
+
+				break;
+			default:
+				break;
+		}
 	}
 
 	//Handling leaves
@@ -128,9 +167,19 @@ void TypeCheck::decorate(ASTNode *node)
 		case NODE_ASSIGNMENT:
 			node->dec = node->children.back()->dec;
 			break;
+		case NODE_EXPRESSION:
+		case NODE_TERM:
+		case NODE_FACTOR:
+			node->dec = node->children.front()->dec;
+			break;
 		default:
+			if (node->children.size() == 0)
+				node->dec = new decoration;
+				node->dec->type = T_NULL;
 			break;
 	}
+
+	return true;
 }
 
 TypeCheck::TypeCheck() {
